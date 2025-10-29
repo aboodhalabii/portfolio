e3fff44e680685c7270f9fdf24bebd6f882c710c
aboodhalabii <aboodalhalaby@gmail.com>
2025-10-29 11:32:33 +0300

    add buttn for generate about me text

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask (__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    data = request.args.get("data")
+    return render_template('index.html', data=data)
+
+
+@app.route('/generate', methods=["POST"])
+def generate():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        job_title = request.form["job_title"]
+
+        response = f"{first_name} {last_name} {job_title}"
+    return redirect(url_for("index", data=response))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-
-
-
-    
-
-    
 
